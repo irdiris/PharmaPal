03673320b6c5d863998ccb9faadd3a30b8e18aaf
baka azzedine <bakaazzedine223@gmail.com>
2023-10-17 00:04:30 +0100

    first user classes created

---
+++ src/main/java/com/example/pharmapal/PharmaPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmapal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaPalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pharmapal/PharmaPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmapal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
